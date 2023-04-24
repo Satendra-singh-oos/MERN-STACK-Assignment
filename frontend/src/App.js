32db2c6426dcb96948ec636f75862599041b74c1
Satendra Singh <satendrasingh1325@gmail.com>
2023-04-24 11:11:57 +0530

    added aleart on each query

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,11 +11,27 @@ const App = () => {
     try {
       const response = await axios.get(`http://localhost:9000/${query}`);
       setData(response.data);
+      if(query === 'q1'){
+        alert("This query will find all the Users which have income lower than $5 USD and have a car of brand “BMW” or “Mercedes”.")
+      }
+      if(query === 'q2'){
+        alert("This query will find all the Male Users which have phone price greater than 10,000.")
+      }
+      if(query === 'q3'){
+        alert("This query will find all the Users whose last name starts with “M” and has a quote character length greater than 15 and email includes his/her last name.")
+      }
+      if(query === 'q4'){
+        alert("This query will find all the Users which have a car of brand “BMW”, “Mercedes” or “Audi” and whose email does not include any digit.")
+      }
+      if(query === 'q5'){
+        alert("This query will Show the data of top 10 cities which have the highest number of users and their average income.")
+      }
       setIsLoading(false);
     } catch (error) {
       console.error(error);
     }
   };
+
 
   return (
     <div>
